c98b72d72a1bbe290c974a428331698de70a41f3
Edoardo Oldani <oldaedo@gmail.com>
2025-03-28 17:22:23 +0100

    Update interfaces.rs

--- honey/src/tun/interfaces.rs
+++ honey/src/tun/interfaces.rs
@@ -5,13 +5,15 @@ use tracing::{info, error};
 
 pub fn create_virtual_tun_interface(ipv4_address: Ipv4Addr) {
 
+    // Crea il nome dell'interfaccia TUN usando l'ultimo ottetto dell'IP
     let last_octet = ipv4_address.octets()[3];
     let tun_name = format!("tun{}", last_octet);
     let netmask = "255.255.255.0".parse::<Ipv4Addr>().expect("Error parsing netmask");
 
+    // Crea l'interfaccia TUN
     let _tun = Arc::new(
         Tun::builder()
-            .name(&tun_name)            
+            .name(&tun_name)
             .address(ipv4_address)
             .netmask(netmask)
             .up()                
@@ -20,14 +22,15 @@ pub fn create_virtual_tun_interface(ipv4_address: Ipv4Addr) {
             .pop()
             .unwrap(),
     );
+
+    // Configura iptables per il forwarding tra la TUN interface e main_tun
     let _ = add_iptables_rule(&tun_name);
     info!("TUN interface created: {tun_name}")
-
 }
 
-
-
+// Funzione per aggiungere regole iptables per il forwarding
 fn add_iptables_rule(tun_interface: &str) -> Result<(), String> {
+    // Controlla se la regola esiste già
     let check_result = Command::new("sudo")
         .arg("iptables")
         .arg("-C")
@@ -45,6 +48,7 @@ fn add_iptables_rule(tun_interface: &str) -> Result<(), String> {
         return Ok(());
     }
 
+    // Se la regola non esiste, aggiungila
     let result = Command::new("sudo")
         .arg("iptables")
         .arg("-A")
@@ -62,6 +66,38 @@ fn add_iptables_rule(tun_interface: &str) -> Result<(), String> {
         error!("Couldn't launch iptables command!");
         return Err(format!("Comando iptables non riuscito: {}", String::from_utf8_lossy(&result.stderr)));
     }
-    
+
+    // Attiva il NAT (Network Address Translation) per la main_tun
+    let nat_result = Command::new("sudo")
+        .arg("iptables")
+        .arg("-t")
+        .arg("nat")
+        .arg("-A")
+        .arg("POSTROUTING")
+        .arg("-o")
+        .arg("main_tun")
+        .arg("-j")
+        .arg("MASQUERADE")
+        .output()
+        .map_err(|e| format!("Errore nell'esecuzione di iptables (NAT): {}", e))?;
+
+    if !nat_result.status.success() {
+        error!("Couldn't launch iptables NAT command!");
+        return Err(format!("Comando iptables NAT non riuscito: {}", String::from_utf8_lossy(&nat_result.stderr)));
+    }
+
+    // Abilita il forwarding IP
+    let forwarding_result = Command::new("sudo")
+        .arg("sysctl")
+        .arg("-w")
+        .arg("net.ipv4.ip_forward=1")
+        .output()
+        .map_err(|e| format!("Errore nell'attivazione del forwarding IP: {}", e))?;
+
+    if !forwarding_result.status.success() {
+        error!("Couldn't enable IP forwarding!");
+        return Err(format!("Errore nell'attivazione del forwarding IP: {}", String::from_utf8_lossy(&forwarding_result.stderr)));
+    }
+
     Ok(())
 }
