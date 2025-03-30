e9e1483a012e3c08ed12a69342edd4d8dae2f889
Edoardo Oldani <oldaedo@gmail.com>
2025-03-30 18:39:42 +0200

    Change bridge mac

--- honey/src/tun/interface.rs
+++ honey/src/tun/interface.rs
@@ -34,6 +34,7 @@ pub async fn send_tun_reply(reply_packet: Vec<u8>, virtual_mac: MacAddr, virtual
 
         println!("Bytes sent: {:?}", bytes);
         run_command("brctl", vec!["delif", "br0", &tun_name]).await;
+        run_command("nft", vec!["del", "rule", "inet", "filter", "input", "iifname", &tun_name, "accept"]).await;
 
         //remove_forwarding_rule(&tun_name, &router_ip).await;
     });
@@ -43,9 +44,9 @@ pub async fn send_tun_reply(reply_packet: Vec<u8>, virtual_mac: MacAddr, virtual
 
 
 async fn change_mac_tun(tun_name: &str, virtual_mac: MacAddr, router_ip: &Ipv4Addr){
-    run_command("ifconfig", vec![tun_name, "hw", "ether", &virtual_mac.to_string()]).await;
+    run_command("ifconfig", vec!["br0", "hw", "ether", &virtual_mac.to_string()]).await;
     run_command("brctl", vec!["addif", "br0", tun_name]).await;
-    run_command("nft", vec!["add", "rule", "inet", "filter", "input", "iifname", tun_name, "accept"]).await;
+    //run_command("nft", vec!["add", "rule", "inet", "filter", "input", "iifname", tun_name, "accept"]).await;
 }
 
 
