dae3f9c1e4554adbda066d62fcbb158905cd37e2
Edoardo Oldani <oldaedo@gmail.com>
2025-03-30 11:51:19 +0200

    Update interface.rs

--- honey/src/tun/interface.rs
+++ honey/src/tun/interface.rs
@@ -38,6 +38,8 @@ pub async fn send_tun_reply(reply_packet: Vec<u8>, virtual_mac: MacAddr, virtual
 
 async fn change_mac_tun(tun_name: &str, virtual_mac: MacAddr, router_ip: &Ipv4Addr){
     run_command("ifconfig", vec![tun_name, "hw", "ether", &virtual_mac.to_string()]);
+    run_command("iptables", vec!["-t", "nat", "-A", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"]).await;
+
     add_forwarding_rule(&tun_name, &router_ip).await;
 
 }
@@ -46,7 +48,9 @@ async fn change_mac_tun(tun_name: &str, virtual_mac: MacAddr, router_ip: &Ipv4Ad
 async fn add_forwarding_rule(interface: &str, router_ip: &Ipv4Addr) -> Result<(), Box<dyn Error>> {
     run_command("iptables", vec!["-A", "FORWARD", "-i", interface, "-o", "eth0", "-j", "ACCEPT"]).await?;
     run_command("iptables", vec!["-A", "FORWARD", "-i", "eth0", "-o", interface, "-j", "ACCEPT"]).await?;
-    run_command("iptables", vec!["-t", "nat", "-A", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"]).await?;
+
+    println!("\nList: {}", run_command("iptables", vec!["-L", "-n", "-v"]).await?);
+    println!("nat: {}", run_command("iptables", vec!["-t", "nat", "-L", "-n", "-v"]).await?);
 
     Ok(())
 }
@@ -54,21 +58,23 @@ async fn add_forwarding_rule(interface: &str, router_ip: &Ipv4Addr) -> Result<()
 async fn remove_forwarding_rule(interface: &str, router_ip: &Ipv4Addr) -> Result<(), Box<dyn Error>> {
     run_command("iptables", vec!["-D", "FORWARD", "-i", interface, "-o", "eth0", "-j", "ACCEPT"]).await?;
     run_command("iptables", vec!["-D", "FORWARD", "-i", "eth0", "-o", interface, "-j", "ACCEPT"]).await?;
-    run_command("iptables", vec!["-t", "nat", "-D", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"]).await?;
 
     Ok(())
 }
 
 
 
-async fn run_command(command: &str, args: Vec<&str>) -> Result<(), Box<dyn Error>> {
+async fn run_command(command: &str, args: Vec<&str>) -> Result<String, Box<dyn Error>> {
     let result = Command::new(command)
         .args(args)
         .output()
         .await;
 
     match result {
-        Ok(output) if output.status.success() => Ok(()),
+        Ok(output) if output.status.success() => {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            Ok(stdout.to_string())
+        },
         Ok(output) => {
             eprintln!("Failed to execute {}: {:?}", command, output);
             Err(format!("Failed to execute {}: {:?}", command, output).into())
