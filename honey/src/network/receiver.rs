69ed05d4e2ade3639946e9f9ec8b4a85e8bbc6f6
Edoardo Oldani <oldaedo@gmail.com>
2025-03-31 11:33:51 +0200

    Update receiver.rs

--- honey/src/network/receiver.rs
+++ honey/src/network/receiver.rs
@@ -89,10 +89,10 @@ pub async fn scan_datalink(
                         }
                     }       
 
-                    
+                    /* 
                     if dest_mac.octets().iter().all(|&byte| byte == 0xFF) {
                         handle_broadcast(&ethernet_packet, &mut *graph, &mut *tx_datalink).await;
-                    }
+                    }*/
 
 
                     
