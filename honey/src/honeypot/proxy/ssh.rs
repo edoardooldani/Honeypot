4fe3d8100747f8b238aea1a3141a405cb47739e9
Edoardo Oldani <oldaedo@gmail.com>
2025-04-09 16:10:22 +0200

    Update ssh.rs

--- honey/src/honeypot/proxy/ssh.rs
+++ honey/src/honeypot/proxy/ssh.rs
@@ -152,6 +152,37 @@ async fn handle_sshd(
             Some(packet_from_client) => {
                 if let Some(tcp_packet) = TcpPacket::new(&packet_from_client) {
                     println!("Pacchetto che arriva: {:?}", tcp_packet.payload());
+
+                    const MAX_PACKET_SIZE: usize = 1024;  // Ad esempio, inviamo pacchetti di dimensione max di 1024 byte
+    
+                    let mut start = 0;
+                    while start < tcp_packet.payload().len() {
+                        let end = std::cmp::min(start + MAX_PACKET_SIZE, tcp_packet.payload().len());
+                        let chunk = &tcp_packet.payload()[start..end];
+                        if let Err(e) = stream.write_all(chunk).await {
+                            error!("❌ Errore nell’invio dati a sshd: {}", e);
+                            let mut sessions = SSH_SESSIONS.lock().await;
+                            sessions.remove(&(virtual_ip, destination_ip));
+                    
+                            let fin_flags = TcpFlags::ACK | TcpFlags::FIN;
+                            send_tcp_stream(
+                                tx.clone(),
+                                virtual_mac,
+                                virtual_ip,
+                                destination_mac,
+                                destination_ip,
+                                22,
+                                tcp_packet.get_source(),
+                                tcp_packet.get_acknowledgement(),
+                                tcp_packet.get_sequence() + tcp_packet.payload().len() as u32,
+                                fin_flags,
+                                &[],
+                            ).await;
+                            break;
+                        }  
+                        start = end;  // Avanza al prossimo blocco
+                    }
+                    /*
                     if let Err(e) = stream.write_all(&tcp_packet.payload()).await {
                         error!("❌ Errore nell’invio dati a sshd: {}", e);
                         let mut sessions = SSH_SESSIONS.lock().await;
@@ -173,7 +204,7 @@ async fn handle_sshd(
                         ).await;
                         break;
                     }
-
+                    */
                     let mut sshd_response = [0u8; 2048];
                     loop{
                         sleep(Duration::from_millis(50)).await;
