751e41abb7e13a66ed2be857f5da2a5b50c67c34
Edoardo Oldani <oldaedo@gmail.com>
2025-07-11 12:22:17 +0200

    Update receiver.rs

--- honey/src/network/receiver.rs
+++ honey/src/network/receiver.rs
@@ -51,67 +51,69 @@ pub async fn scan_datalink(
     let local_mac = get_local_mac();
 
     loop {
-        let packet = match rx.next() {
-            Ok(packet) => packet,
+        match rx.next() {
+            Ok(packet) => {
+
+                if let Some(ethernet_packet) = EthernetPacket::new(packet) {
+                    let src_mac = ethernet_packet.get_source();
+
+                    if src_mac == local_mac {
+                        continue;
+                    }
+
+                    if let Some(ethernet_packet) = EthernetPacket::new(&packet.to_vec()) {
+                        detect_anomaly(ai_model.clone(), ethernet_packet).await;
+                    }
+
+                    let dest_ip: Ipv4Addr = update_graph_from_packet(graph.clone(), &ethernet_packet, packet.len()).await;
+                    
+                    let graph_guard = graph.lock().await;
+
+                    // Handle virtual node
+                    if let Some(dest_node) = graph_guard.find_virtual_node_by_ip_or_mac(ethernet_packet.get_destination(), dest_ip) {
+                        let tx_clone = tx_datalink.clone();
+                        let ethertype = ethernet_packet.get_ethertype();
+                        let payload = ethernet_packet.payload().to_vec();
+                        let source = ethernet_packet.get_source();
+                        let virtual_mac = dest_node.mac_address.clone();
+                        let virtual_ip = dest_node.ipv4_address.clone();
+
+                        tokio::spawn(async move {
+                            handle_virtual_packet(
+                                ethertype,
+                                payload,
+                                &source,
+                                &virtual_mac, 
+                                &virtual_ip, 
+                                tx_clone
+                            ).await;  
+                        });
+                                
+                    }
+
+                    let mut graph_lock = graph.lock().await;
+
+                    detect_attacks(
+                        tx.clone(), 
+                        session_id.clone(), 
+                        &ethernet_packet, 
+                        &mut graph_lock, 
+                        local_mac.clone(), 
+                        arp_req_alert_tracker.clone(),
+                        arp_res_alert_tracker.clone(),
+                        Arc::clone(&arp_req_tracker), 
+                        Arc::clone(&arp_res_tracker), 
+                        tcp_syn_tracker.clone(),
+                    ).await;
+
+                }
+
+        },
             Err(e) => {
                 eprintln!("❌ Errore nella lettura del pacchetto: {}", e);
                 continue;
             }
         };
-
-        if let Some(ethernet_packet) = EthernetPacket::new(packet) {
-            let src_mac = ethernet_packet.get_source();
-
-            if src_mac == local_mac {
-                continue;
-            }
-
-            if let Some(ethernet_packet) = EthernetPacket::new(&packet.to_vec()) {
-                detect_anomaly(ai_model.clone(), ethernet_packet).await;
-            }
-
-            let dest_ip: Ipv4Addr = update_graph_from_packet(graph.clone(), &ethernet_packet, packet.len()).await;
-            
-            let graph_guard = graph.lock().await;
-
-            // Handle virtual node
-            if let Some(dest_node) = graph_guard.find_virtual_node_by_ip_or_mac(ethernet_packet.get_destination(), dest_ip) {
-                let tx_clone = tx_datalink.clone();
-                let ethertype = ethernet_packet.get_ethertype();
-                let payload = ethernet_packet.payload().to_vec();
-                let source = ethernet_packet.get_source();
-                let virtual_mac = dest_node.mac_address.clone();
-                let virtual_ip = dest_node.ipv4_address.clone();
-
-                tokio::spawn(async move {
-                    handle_virtual_packet(
-                        ethertype,
-                        payload,
-                        &source,
-                        &virtual_mac, 
-                        &virtual_ip, 
-                        tx_clone
-                    ).await;  
-                });
-                        
-            }
-
-            let mut graph_lock = graph.lock().await;
-
-            detect_attacks(
-                tx.clone(), 
-                session_id.clone(), 
-                &ethernet_packet, 
-                &mut graph_lock, 
-                local_mac.clone(), 
-                arp_req_alert_tracker.clone(),
-                arp_res_alert_tracker.clone(),
-                Arc::clone(&arp_req_tracker), 
-                Arc::clone(&arp_res_tracker), 
-                tcp_syn_tracker.clone(),
-            ).await;
-
-        }
     }
 }
 
