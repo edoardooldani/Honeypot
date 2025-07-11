69f4e8d1b1698a810321c6640d28ace67786580b
Edoardo Oldani <oldaedo@gmail.com>
2025-07-11 16:41:02 +0200

    Refactor flow

--- honey/src/AI/detection.rs
+++ honey/src/AI/detection.rs
@@ -1,17 +1,6 @@
 use tract_onnx::prelude::*;
-use pnet::packet::{ethernet::EthernetPacket, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, udp::UdpPacket};
-use std::collections::HashMap;
-use std::sync::Mutex as StdMutex;
-use lazy_static::lazy_static;
-use pnet::packet::tcp::TcpPacket;
-use pnet::packet::Packet;
-use crate::{trackers::flow::{FlowKey, FlowTracker}, ai::features::PacketFeatures};
-
-lazy_static! {
-    static ref FLOW_TRACKER: StdMutex<FlowTracker> = StdMutex::new(FlowTracker {
-        flows: HashMap::new(),
-    });
-}
+use pnet::packet::ethernet::EthernetPacket;
+use crate::trackers::flow::get_packet_flow;
 
 pub async fn detect_anomaly<'a>(
     model: SimplePlan<TypedFact, Box<dyn TypedOp>, tract_onnx::prelude::Graph<TypedFact, Box<dyn TypedOp>>>, 
@@ -23,49 +12,7 @@ pub async fn detect_anomaly<'a>(
     if packet_features.is_none() {
         return false; // No tcp/udp found, no anomaly to detect
     }
+
     println!("Packet Features: {:?}", packet_features);
     false
-}
-
-async fn get_packet_flow<'a>(ethernet_packet: &EthernetPacket<'a>) -> Option<PacketFeatures> {
-    if let Some(ip_packet) = Ipv4Packet::new(ethernet_packet.payload()) {
-        let src_ip = ip_packet.get_source().to_string();
-        let dst_ip = ip_packet.get_destination().to_string();
-        let protocol = ip_packet.get_next_level_protocol();
-
-        if protocol == IpNextHeaderProtocols::Tcp {
-            if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
-                let src_port = tcp_packet.get_source();
-                let dst_port = tcp_packet.get_destination();
-
-                let key = FlowKey {
-                    ip_src: src_ip,
-                    ip_dst: dst_ip,
-                    port_src: src_port,
-                    port_dst: dst_port,
-                    protocol: 6,
-                };
-
-                return Some(FLOW_TRACKER.lock().unwrap().get_flow_or_insert(key).clone());
-            }
-        }
-        else if protocol == IpNextHeaderProtocols::Udp {
-            if let Some(udp_packet) = UdpPacket::new(ip_packet.payload()) {
-                let src_port = udp_packet.get_source();
-                let dst_port = udp_packet.get_destination();
-
-                let key = FlowKey {
-                    ip_src: src_ip,
-                    ip_dst: dst_ip,
-                    port_src: src_port,
-                    port_dst: dst_port,
-                    protocol: 17,
-                };
-                
-                return Some(FLOW_TRACKER.lock().unwrap().get_flow_or_insert(key).clone());
-            }
-        }
-    }
-    
-    None
 }
--- honey/src/trackers/flow.rs
+++ honey/src/trackers/flow.rs
@@ -1,5 +1,12 @@
 use std::collections::HashMap;
+use std::time::Instant;
 use crate::ai::features::PacketFeatures;
+use lazy_static::lazy_static;
+use pnet::packet::{ethernet::EthernetPacket, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, udp::UdpPacket};
+use std::sync::Mutex;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::Packet;
+
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PacketDirection {
@@ -24,4 +31,53 @@ impl FlowTracker {
     pub fn get_flow_or_insert(&mut self, key: FlowKey) -> &PacketFeatures {
         self.flows.entry(key).or_insert_with(PacketFeatures::default)
     }
+}
+
+lazy_static! {
+    static ref FLOW_TRACKER: Mutex<FlowTracker> = Mutex::new(FlowTracker {
+        flows: HashMap::new(),
+    });
+}
+
+pub async fn get_packet_flow<'a>(ethernet_packet: &EthernetPacket<'a>) -> Option<PacketFeatures> {
+    if let Some(ip_packet) = Ipv4Packet::new(ethernet_packet.payload()) {
+        let src_ip = ip_packet.get_source().to_string();
+        let dst_ip = ip_packet.get_destination().to_string();
+        let protocol = ip_packet.get_next_level_protocol();
+
+        if protocol == IpNextHeaderProtocols::Tcp {
+            if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
+                let src_port = tcp_packet.get_source();
+                let dst_port = tcp_packet.get_destination();
+
+                let key = FlowKey {
+                    ip_src: src_ip,
+                    ip_dst: dst_ip,
+                    port_src: src_port,
+                    port_dst: dst_port,
+                    protocol: 6,
+                };
+
+                return Some(FLOW_TRACKER.lock().unwrap().get_flow_or_insert(key).clone());
+            }
+        }
+        else if protocol == IpNextHeaderProtocols::Udp {
+            if let Some(udp_packet) = UdpPacket::new(ip_packet.payload()) {
+                let src_port = udp_packet.get_source();
+                let dst_port = udp_packet.get_destination();
+
+                let key = FlowKey {
+                    ip_src: src_ip,
+                    ip_dst: dst_ip,
+                    port_src: src_port,
+                    port_dst: dst_port,
+                    protocol: 17,
+                };
+                
+                return Some(FLOW_TRACKER.lock().unwrap().get_flow_or_insert(key).clone());
+            }
+        }
+    }
+    
+    None
 }
