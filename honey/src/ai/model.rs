b0b53e2c5f4f933dca978111b631351e79e6a53e
Edoardo Oldani <oldaedo@gmail.com>
2025-07-16 18:05:22 +0200

    Update model.rs

--- honey/src/ai/model.rs
+++ honey/src/ai/model.rs
@@ -1,4 +1,4 @@
-use tract_onnx::prelude::*;
+use tract_onnx::{prelude::*, tract_core::ndarray::ArrayView2};
 
 pub fn load_model() -> SimplePlan<TypedFact, Box<dyn TypedOp>, tract_onnx::prelude::Graph<TypedFact, Box<dyn TypedOp>>>{
     let model = tract_onnx::onnx()
@@ -10,9 +10,18 @@ pub fn load_model() -> SimplePlan<TypedFact, Box<dyn TypedOp>, tract_onnx::prelu
 
 
 pub fn run_inference(model: &SimplePlan<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box<dyn TypedOp>>>, input_tensor: Tensor) -> TractResult<Tensor> {
-    println!("Running inference with input tensor: {:?}", input_tensor);
+    print_tensor(&input_tensor);
     let result = model.run(tvec!(input_tensor.into())).expect("Failed to run inference");
-    let output_tensor = result[0].clone(); // direttamente il tensore di output, senza scalarizzazione
+    let output_tensor = result[0].clone();
     Ok(output_tensor.into_tensor())
+}
+
+
+fn print_tensor(tensor: &Tensor) {
+    let array: ArrayView2<f32> = tensor.to_array_view::<f32>().unwrap().into_dimensionality::<tract_ndarray::Ix2>().unwrap();
+
+    for row in array.rows() {
+        println!("{:?}", row);
+    }
 }
 
