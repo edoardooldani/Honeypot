74d5c28de74b920e6ed845968d0e087f3407a288
Edoardo Oldani <oldaedo@gmail.com>
2025-07-20 10:24:47 +0200

    Update detection.rs

--- honey/src/ai/detection.rs
+++ honey/src/ai/detection.rs
@@ -37,14 +37,14 @@ pub async fn detect_anomaly<'a>(
         .expect("Errore nella normalizzazione");
 
 
-    //let array = feature_tensors.to_array_view::<f32>().unwrap();
-    //let cloned_array = array.to_owned(); // Se ti serve conservarlo
+    let array = feature_tensors.to_array_view::<f32>().unwrap();
+    let cloned_array = array.to_owned(); // Se ti serve conservarlo
 
     
     if should_evaluate(&packet_features.clone()){
         match run_autoencoder_inference(&autoencoder, feature_tensors) {
             Ok(result) => {
-                if result > 1 {
+                if result > 1.0 {
                     println!("\nNormalized tensor");
                     for elem in cloned_array {
                         println!("{:?}", elem);
