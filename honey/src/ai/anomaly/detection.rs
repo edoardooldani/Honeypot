594fe82970676ca746e947b69ced03fb144df95f
Edoardo Oldani <oldaedo@gmail.com>
2025-07-20 18:43:52 +0200

    Update detection.rs

--- honey/src/ai/anomaly/detection.rs
+++ honey/src/ai/anomaly/detection.rs
@@ -57,7 +57,7 @@ pub fn classify_anomaly(
     match run_classifier_inference(&classifier, feature_tensors) {
         Ok(score) => {
             if score != 0 {
-                warn!("Anomaly score: {}", score);
+                warn!("Anomaly score: {:?}", AnomalyClassification::from_index(score as u8));
                 return AnomalyClassification::from_index(score as u8);
             }else {
                 println!("Benign classified: {:?}", score);
