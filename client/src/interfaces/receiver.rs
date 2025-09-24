275b73d9b4111dbf5c7b0bea5159c28e79e6f605
Edoardo Oldani <oldaedo@gmail.com>
2025-09-24 11:42:36 +0200

    Deadlock fix on anomalies detected

--- client/src/interfaces/receiver.rs
+++ client/src/interfaces/receiver.rs
@@ -1,6 +1,5 @@
-use common::packet::build_packet;
-use common::packet_features::PacketFeatures;
-use common::types::{AlertPayload, Packet as WsPacket, PayloadType};
+use common::packet::{build_header, build_packet};
+use common::types::{AlertPayload, PayloadType};
 use pnet::datalink::{self, Channel, Config, DataLinkSender, NetworkInterface};
 use pnet::packet::ethernet::EthernetPacket;
 use pnet::packet::Packet;
@@ -9,8 +8,6 @@ use tokio_tungstenite::tungstenite::protocol::Message;
 use tracing::{info, error};
 use tokio::sync::Mutex;
 use tract_onnx::prelude::*;
-use std::net::Ipv4Addr;
-use std::ops::Add;
 use std::sync::Arc;
 use crate::ai::anomaly::anomalies::AnomalyClassification;
 use crate::ai::features::flow::get_flow;
@@ -67,61 +64,71 @@ async fn scan_packet(
     session_id: Arc<Mutex<u32>>,
 ) {
     if let Some(ethernet_packet) = EthernetPacket::new(packet) {                    
-        let mut graph_lock = graph.lock().await;
         
-        let (src_node, dest_node) = graph_lock.add_nodes_and_connections(&ethernet_packet, local_mac).await;
-        
-        if dest_node.node_type == NodeType::Virtual {
+        let (src_mac, src_ip, dest_is_virtual) = {
+            let mut g = graph.lock().await;
+            let (src_node, dest_node) = g.add_nodes_and_connections(&ethernet_packet, local_mac).await;
+
+            (src_node.mac_address, src_node.ipv4_address, dest_node.node_type == NodeType::Virtual)
+        };
+
+        if dest_is_virtual {
             let tx_clone = tx_datalink.clone();
             let packet_data = ethernet_packet.packet().to_vec();
-
             tokio::spawn(async move {
                 let packet = EthernetPacket::new(&packet_data).unwrap();
                 info!("🤖 Handling virtual honeypot packet for {:?}", packet.get_destination());
-                handle_virtual_packet(packet, tx_clone).await;  
+                handle_virtual_packet(packet, tx_clone).await;
             });
         }
+
         
         let packet_data = ethernet_packet.packet().to_vec();
         let packet_ethernet = EthernetPacket::new(&packet_data).unwrap();
-
         let classification = detect_anomaly(Arc::clone(&autoencoder_model), Arc::clone(&classifier_model), packet_ethernet).await;   
+        
         if classification != AnomalyClassification::Benign {
-            info!("⚠️ Anomaly detected: {:?} from {:?}", classification, src_node.clone());
+            info!("⚠️ Anomaly detected: {:?} from {:?}", classification, src_ip);
 
-            let mut graph_lock = graph.lock().await;
-            let anomalies_length = graph_lock.add_anomaly(&ethernet_packet, classification);
+            let (anomalies_length, msg) = {
+                let mut g = graph.lock().await;
+                let anomalies_length = g.add_anomaly(&ethernet_packet, classification);
 
-            let msg_packet: WsPacket = build_ws_alert(get_flow(&ethernet_packet).await.expect("Failed to extract packet features when anomalies detected"), 
-                session_id, 
-                src_node.mac_address,
-                src_node.ipv4_address,
-                anomalies_length
-            ).await;
+                let features = get_flow(&ethernet_packet)
+                    .await
+                    .expect("Failed to extract packet features when anomalies detected");
 
-            let serialized = bincode::serialize(&msg_packet).expect("Errore nella serializzazione");
-            let msg = Message::Binary(serialized.into());
-            info!("📤 Sending alert to server, packet ID: {}, anomalies count: {}", msg_packet.header.id, anomalies_length);
+                let id = {
+                    let mut id_lock = session_id.lock().await;
+                    *id_lock += 1;
+                    *id_lock
+                };
 
+                let priority = if anomalies_length > 10 { 3 }
+                               else if anomalies_length > 5 { 2 }
+                               else { 1 };
+
+                let header = build_header(id, 1, priority, src_mac);
+                let ws_packet = build_packet(
+                    header,
+                    PayloadType::Alert(AlertPayload {
+                        mac_address: src_mac.octets(),
+                        ip_address: src_ip.expect("Failed to extract ip").to_string(),
+                        features
+                    })
+                );
+
+                let serialized = bincode::serialize(&ws_packet).expect("serialize error");
+                let msg = Message::Binary(serialized.into());
+                (anomalies_length, msg)
+            }; 
+
+            info!("📤 Sending alert to server, anomalies count: {}", anomalies_length);
             ws_tx.unbounded_send(msg).unwrap();
-        }else {
+        }
+        else {
             //info!("✔️ Benign packet from {:?}", src_node.clone());
         }
     }
 
-}
-
-
-async fn build_ws_alert(
-    features: PacketFeatures, 
-    session_id: Arc<Mutex<u32>>, 
-    mac_address: MacAddr,
-    ip_address: Option<Ipv4Addr>,
-    anomalies_length: usize
-)-> WsPacket {
-    let priority = if anomalies_length > 10 { 3 } else if anomalies_length > 5 { 2 } else { 1 };
-    let id = session_id.lock().await.add(1);
-    let header = common::packet::build_header(id, 1, priority, mac_address);
-
-    build_packet(header, PayloadType::Alert(AlertPayload { mac_address: mac_address.octets(), ip_address: ip_address.expect("Failed to extract ip").to_string(), features }))
 }
